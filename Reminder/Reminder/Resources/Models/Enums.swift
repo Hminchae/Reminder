71590522241d104fec66ec1018f83b50946fb1cf
Hminchae <hminchae@gmail.com>
2024-07-04 19:01:42 +0900

    Feat : Sort 를 위한 Enum 추가

--- Reminder/Reminder/Resources/Models/Enums.swift
+++ Reminder/Reminder/Resources/Models/Enums.swift
@@ -66,18 +66,68 @@ enum View {
             }
         }
     }
-}
-
-enum Value {
-    enum Category: String, CaseIterable { // 이거 나중엔 추가되도록 해야하는데..
+    enum CustomCategory: String, CaseIterable { // 이거 나중엔 추가되도록 해야하는데..
         case reminder = "미리 알림"
         case toDo = "할일"
     }
-    
-    enum listSort: String, CaseIterable {
-        case dueDate = "마감일"
-        case registerDate = "생성일"
-        case priority = "우선 순위"
-        case title = "제목"
+}
+
+enum Sort {
+    enum TypeOf {
+        case dueDate(asc: Bool)
+        case registerDate(asc: Bool)
+        case priority(asc: Bool)
+        case title(asc: Bool)
+        
+        var name: String {
+            switch self {
+            case .dueDate:
+                return "마감일"
+            case .registerDate:
+                return "생성일"
+            case .priority:
+                return "우선 순위"
+            case .title:
+                return "제목"
+            }
+        }
+        
+        var way: String {
+            switch self {
+                
+            case .dueDate(let asc):
+                switch asc {
+                case true:
+                    return "늦은 항목 순으로"
+                case false:
+                    return "이른 항목 순으로"
+                }
+                
+            case .registerDate(let asc):
+                switch asc {
+                case true:
+                    return "최신 항목 순으로"
+                case false:
+                    return "오래된 항목 순으로"
+                }
+                
+            case .priority(let asc):
+                switch asc {
+                case true:
+                    return "낮은 우선 순위로"
+                case false:
+                    return "높은 우선 순위로"
+                }
+                
+            case .title(let asc):
+                switch asc {
+                case true:
+                    return "늦은 항목 순으로"
+                case false:
+                    return "이른 항목 순으로"
+                }
+            }
+        }
     }
 }
+
